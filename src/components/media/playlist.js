7dc6ec163ba350023ae607e64136a1e01e4287fd
Moises Serrano <johndoe@example.com>
2018-09-17 19:11:59 -0400

    composicion de componentes

--- src/components/media/playlist.js
+++ src/components/media/playlist.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Media from './media.js';
+import Play from '../../icons/components/play';
+
 import './playlist.css';
 
 function Playlist(props) {
@@ -8,6 +10,10 @@ function Playlist(props) {
     const playlist = props.data.categories[0].playlist;
     return (
         <div className="Playlist">
+            <Play
+                size={100}
+                color="red"
+            />
             {
                 playlist.map( (item,i)=> {
                     return <Media title={item.title} author={item.author} cover={item.cover} key={item.id} />
