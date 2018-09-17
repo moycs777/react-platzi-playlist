62472e052c6239aa562a29d4c2f65e398532f360
John Doe <johndoe@example.com>
2018-09-17 15:28:25 -0400

    estructura del proyecto

---
+++ src/media.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Media extends Component {
+    render() {
+        return (
+               
+                <p className="App-intro">
+                    Hola sloy el componente media
+                 </p>
+        );
+    }
+}
+
+export default Media;
