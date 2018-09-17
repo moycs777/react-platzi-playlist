e29ced47624f1ca3838749bdada3968a2cb8b631
Moises Serrano <johndoe@example.com>
2018-09-17 17:18:07 -0400

    componente media, playlist, con sus css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { hydrate } from 'react-dom';
+import data from '../src/api.json';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App data={data} />, document.getElementById('root'));
 registerServiceWorker();
+
